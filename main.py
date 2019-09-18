5ab5918184d39d53c0f497674b256be0c710e3fe
mudrakpatel <mudrakpatel96@gmail.com>
2019-09-18 17:34:11 +0530

    Very basic python web crawler done

---
+++ main.py
@@ -0,0 +1,4 @@
+from WebCrawler import getSingleItemData
+
+url = 'https://www.amazon.in/Redux-Analogue-Blue-Watch-RWS0216S/dp/B07KVZD6XM/ref=gbps_tit_s-5_9f8e_118c710a?smid=A2NSMMC7X1GFYE&pf_rd_p=b4500f5f-e496-4b18-ab75-623b14149f8e&pf_rd_s=slot-5&pf_rd_t=701&pf_rd_i=gb_main&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_r=SC8Y60TWE99XEQXBHDEJ'
+getSingleItemData(url)
